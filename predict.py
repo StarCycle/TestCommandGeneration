cdb369fab93deec887204e680a2e8fa1c6d1fb13
Zhuoheng Li <zhuohengli@foxmail.com>
2022-05-24 15:31:41 +0100

    1. remove K fold 2. add other metrics (precision, recall, f1) 3. Add Tensorboard

--- predict.py
+++ predict.py
@@ -1,21 +1,24 @@
 import torch
 from torch import nn
 from torch.utils import data
+from torch.utils.tensorboard import SummaryWriter
 import json
 
-def load_array(data_arrays, batch_size, is_train=True):
-    '''Consruct a PyTorch Dataloader'''
-    dataset = data.TensorDataset(*data_arrays)
-    return data.DataLoader(dataset, batch_size, shuffle=is_train)
-
-def accuracy(net, features, labels):
+def analysis(net, features, labels):
     true_positive = labels[net(features)>0.5].sum().item()
     true_negative = (labels[net(features)<0.5]==0).sum().item()
-    return (true_positive + true_negative) / labels.numel()
+    fault_positive = (labels[net(features)>0.5]==0).sum().item()
+    fault_negative = labels[net(features)<0.5].sum().item()
+    accuracy = (true_positive + true_negative) / labels.numel()
+    precision = true_positive / (true_positive + fault_positive)
+    recall = true_positive / (true_positive + fault_negative)
+    f1 = 2*true_positive / (2*true_positive + fault_positive + fault_negative)
+    return accuracy, precision, recall, f1
 
-def train(net, loss, train_features, train_labels, test_features, test_labels, num_epochs, learning_rate, weight_decay, batch_size, patiance):
+def train(net, loss, trainset, testset, num_epochs, learning_rate, weight_decay, batch_size, patiance):
     train_ls, test_ls = [], []
-    train_iter = load_array((train_features, train_labels), batch_size)
+    log_writer = SummaryWriter()
+    train_iter = data.DataLoader(trainset, batch_size, shuffle=True)
     optimizer = torch.optim.Adam(net.parameters(), lr = learning_rate, weight_decay = weight_decay)
     for epoch in range(num_epochs):
         for X, y in train_iter:
@@ -23,59 +26,30 @@ def train(net, loss, train_features, train_labels, test_features, test_labels, n
             l = loss(net(X), y)
             l.backward()
             optimizer.step()
-        if epoch % 50 == 0:
-            with torch.no_grad():
-                current_train_ls = loss(net(train_features), train_labels).data.item()
-                current_test_ls = loss(net(test_features), test_labels).data.item()
-                current_train_acc = accuracy(net, train_features, train_labels)
-                current_test_acc = accuracy(net, test_features, test_labels)
-                if epoch == 0:
-                    best_test_ls = current_test_ls
-                train_ls.append(current_train_ls)
-                test_ls.append(current_test_ls)
-                print(f'epoch {epoch}: train loss {current_train_ls}, test loss {current_test_ls}, train acc {current_train_acc}, test acc {current_test_acc}')
-                if current_test_ls < best_test_ls:
-                    best_test_ls = current_test_ls
-                    torch.save(net.state_dict(), 'Model')
-                # elif sum(test_ls[-patiance:])/min(len(test_ls), patiance) > best_test_ls:
-                    # break # early stop
-    return train_ls, test_ls
-
-def get_k_fold_data(k, i, X, y):
-    '''
-    Input:
-            k	total fold number
-            i	Which fold is used as testation
-            X 	Feature vectors. The first dimension is number of samples
-            y 	label vectors.
-    Output:
-            Training set and testation set of fold i
-    '''
-    assert k > 1
-    fold_size = X.shape[0] // k
-    X_train, y_train = None, None
-    for j in range(k):
-        idx = slice(j * fold_size, (j + 1) * fold_size)
-        X_part, y_part = X[idx, :], y[idx]
-        if j == i:
-            X_test, y_test = X_part, y_part
-        elif X_train is None:
-            X_train, y_train = X_part, y_part
-        else:
-            X_train = torch.cat([X_train, X_part], 0)
-            y_train = torch.cat([y_train, y_part], 0)
-    return X_train, y_train, X_test, y_test
-
-def k_fold(k, net, loss, X_train, y_train, num_epochs, learning_rate, weight_decay, batch_size, patience):
-    train_ls_sum, test_ls_sum = 0, 0
-    for i in range(k):
-        X_train, y_train, X_test, y_test = get_k_fold_data(k, i, X_train, y_train)
-        net.apply(my_init)
-        train_ls, test_ls = train(net, loss, X_train, y_train, X_test, y_test, num_epochs, learning_rate, weight_decay, batch_size, patience)
-        train_ls_sum += train_ls[-1]
-        test_ls_sum += test_ls[-1]
-    # Return average loss on the training set and testing set
-    return train_ls_sum / k, test_ls_sum / k
+        with torch.no_grad():
+            train_features, train_labels = trainset[:]
+            test_features, test_labels = testset[:]
+            cur_train_ls = loss(net(train_features), train_labels).data.item()
+            cur_test_ls = loss(net(test_features), test_labels).data.item()
+            cur_train_acc, cur_train_prec, cur_train_recall, cur_train_f1 = analysis(net, train_features, train_labels)
+            cur_test_acc, cur_test_prec, cur_test_recall, cur_test_f1 = analysis(net, test_features, test_labels)
+            train_ls.append(cur_train_ls)
+            test_ls.append(cur_test_ls)
+            log_writer.add_scalars('loss', {'train loss': cur_train_ls, 'test loss': cur_test_ls}, epoch)
+            log_writer.add_scalars('accuracy', {'train acc': cur_train_acc, 'test acc': cur_test_acc}, epoch)
+            log_writer.add_scalars('precison', {'train prec': cur_train_prec, 'test prec': cur_test_prec}, epoch)
+            log_writer.add_scalars('recall', {'train recall': cur_train_recall, 'test recall': cur_test_recall}, epoch)
+            log_writer.add_scalars('f1', {'train f1': cur_train_f1, 'test f1': cur_test_f1}, epoch)
+            print(f'epoch {epoch}: train loss {cur_train_ls}, test loss {cur_test_ls}')
+        # Early stop
+        if epoch % 20 == 0:
+            if epoch == 0:
+                best_test_ls = cur_test_ls
+            if cur_test_ls < best_test_ls:
+                best_test_ls = cur_test_ls
+                torch.save(net.state_dict(), 'Model')
+            elif sum(test_ls[-patiance:])/min(len(test_ls), patiance) > best_test_ls:
+                break
 
 def my_init(module):
     if type(module) == nn.Linear:
@@ -107,12 +81,13 @@ net = nn.Sequential(nn.Linear(in_features, 500),
                     nn.ReLU(),
                     nn.Linear(500, out_features))
 net = net.to(device=try_gpu())
+net.apply(my_init)
 
 # Train
-k, num_epochs, lr, weight_decay, batch_size, patience = 5, 1000, 5, 0, 64, 5
-train_l, test_l = k_fold(k, net, loss, cmds, cov, num_epochs, lr, weight_decay, batch_size, patience)
-
-# Plot
-predicted_cov = net(cmds)
+num_epochs, lr, weight_decay, batch_size, patience, split = 1000, 5, 0, 64, 5, 0.8
+dataset = data.TensorDataset(cmds, cov)
+trainset_size = int(len(dataset)*0.8) 
+trainset, testset = data.random_split(dataset, [trainset_size, int(len(dataset)) - trainset_size])
+train(net, loss, trainset, testset, num_epochs, lr, weight_decay, batch_size, patience)
 
 print('here')
