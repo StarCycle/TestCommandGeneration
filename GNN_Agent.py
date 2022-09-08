b6ae76fe33c59a844f519c57c895ae2f15a8750d
Li Li <llizhuoheng@tudelft.nl>
2022-09-09 00:56:36 +0200

    GNN converges byt not very good

--- GNN_Agent.py
+++ GNN_Agent.py
@@ -1,6 +1,6 @@
 import json
-import numpy as np
 import torch
+import numpy as np
 from torch import nn
 from torch import Tensor 
 from torch.nn import functional as F
@@ -8,21 +8,32 @@ from torch.distributions.categorical import Categorical
 from torch_geometric.nn.inits import uniform
 from torch_geometric.nn.conv import MessagePassing
 
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
 class GatedGraphConv(MessagePassing):
     
-    def __init__(self, channel_num, num_layers, aggr = 'add', bias = True, **kwargs):
+    def __init__(self, out_channels, num_layers, aggr = 'add', bias = True, **kwargs):
         super(GatedGraphConv, self).__init__(aggr=aggr, **kwargs)
-        self.channel_num = channel_num
+        self.out_channels = out_channels
         self.num_layers = num_layers
-        self.weight = nn.Parameter(Tensor(num_layers, channel_num, channel_num))
-        self.rnn = torch.nn.GRUCell(channel_num, channel_num, bias=bias)
+        self.weight = nn.Parameter(Tensor(num_layers, out_channels, out_channels))
+        self.rnn = torch.nn.GRUCell(out_channels, out_channels, bias=bias)
         self.reset_parameters()
 		
     def reset_parameters(self):
-        uniform(self.channel_num, self.weight)
+        uniform(self.out_channels, self.weight)
         self.rnn.reset_parameters()
 
     def forward(self, x, edge_index):
+        if x.size(-1) > self.out_channels:
+            raise ValueError('The number of input channels is not allowed to '
+                             'be larger than the number of output channels')
+        if x.size(-1) < self.out_channels:
+            fill = x[:,-1].repeat(self.out_channels-x.shape[-1], 1)
+            x = torch.cat([x, fill.T], dim=1)
         for i in range(self.num_layers):
           m = torch.matmul(x, self.weight[i])
           m = self.propagate(edge_index, x=m, edge_weight=None, size=None)
@@ -32,45 +43,33 @@ class GatedGraphConv(MessagePassing):
     def message(self, x_j):
         return x_j
 
-def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-    torch.nn.init.orthogonal_(layer.weight, std)
-    torch.nn.init.constant_(layer.bias, bias_const)
-    return layer
-
 class GNN_Agent(torch.nn.Module):
-    def __init__(self, env, graphFile):
+    def __init__(self, env, graphFile, out_channels, gnn_layers, device):
         super(GNN_Agent, self).__init__()
         with open('graph.json', 'r') as f:
             graph = json.load(f)
         self.nodes = torch.tensor(graph['features'], dtype=torch.float32)
+        self.nodes = torch.zeros(self.nodes.shape)
         self.edges = torch.tensor(graph['edges'], dtype=torch.long).T
-        self.count2label = torch.tensor(graph['count2label'], dtype=torch.long)			
-        nodeNum = self.nodes.shape[0]
-        nodeChannels = self.nodes.shape[1] + 1 # Add Code cov info 
-        self.graphConv = GatedGraphConv(nodeChannels, 3)
-        self.critic = nn.Sequential(
-                      layer_init(nn.Linear(631, 1024)),
-                      nn.ReLU(),
-                      layer_init(nn.Linear(1024, 1024)),
-					  nn.ReLU(),
-					  layer_init(nn.Linear(1024, 1), std=1))
-        self.actor = nn.Sequential(
-                     layer_init(nn.Linear(631, 1024)),
-                     nn.ReLU(),
-					 layer_init(nn.Linear(1024, 1024)),
-					 nn.ReLU(),
-                     layer_init(nn.Linear(1024, len(env.actions)), std=0.01))
+        self.count2label = torch.tensor(graph['count2label'], dtype=torch.long)
+        self.historyVecLen = len(env.actions)			
+        self.graphConv = GatedGraphConv(out_channels, gnn_layers)
+        self.nodemlp = nn.Sequential(
+            layer_init(nn.Linear(out_channels, 1)),
+            nn.ReLU(),
+        )
+        self.graphmlp = nn.Sequential(
+            layer_init(nn.Linear(self.nodes.shape[0]+self.historyVecLen, 2048)),
+            nn.ReLU(),
+        )
+        self.critic = layer_init(nn.Linear(2048, 1), 1)
+        self.actor = layer_init(nn.Linear(2048, len(env.actions)), 0.01)
         
-    def GNN(self, cov):
-        if len(cov.shape) == 1:
-            batchSize = 1
-            cov = cov.reshape((1, -1))
-        else:
-            batchSize = cov.shape[0]
+    def GNN(self, cov, batchNum):
         device = cov.device
         nodesInput = torch.tensor([]).to(device)
         edgesInput = torch.tensor([], dtype=torch.long).to(device)
-        for i in range(batchSize):
+        for i in range(batchNum):
             covInGraph = torch.zeros(self.nodes.shape[0]).to(device) 
             covInGraph[self.count2label] = cov[i] 
             covInGraph = covInGraph.reshape((-1, 1))
@@ -78,16 +77,35 @@ class GNN_Agent(torch.nn.Module):
             nodesInput = torch.cat((nodesInput, temp), 0)
             temp = self.edges + i*self.nodes.shape[0]
             edgesInput = torch.cat((edgesInput, temp), 1)
-        x = self.graphConv(nodesInput, edgesInput)
-        x = x.reshape((batchSize, -1))
+        x = self.graphConv(nodesInput, edgesInput) 
+        x = self.nodemlp(x)
+        x = x.reshape((batchNum, -1))
         return x
 		
     def get_value(self, x):
-        return self.critic(x)
+        if len(x.shape) == 1:
+            batchNum = 1
+            x = x.reshape((1, -1))
+        else:
+            batchNum = x.shape[0]
+        cov = x[:, :len(self.count2label)]
+        history = x[:, len(self.count2label):]
+        nodeData = self.GNN(cov, batchNum)
+        state = self.graphmlp(torch.cat((nodeData, history), 1))
+        return self.critic(state)
 
     def get_action_and_value(self, x, action=None):
-        logits = self.actor(x)
+        if len(x.shape) == 1:
+            batchNum = 1
+            x = x.reshape((1, -1))
+        else:
+            batchNum = x.shape[0]
+        cov = x[:, :len(self.count2label)]
+        history = x[:, len(self.count2label):]
+        nodeData = self.GNN(cov, batchNum)
+        state = self.graphmlp(torch.cat((nodeData, history), 1))
+        logits = self.actor(state)
         probs = Categorical(logits=logits)
         if action is None:
             action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
+        return action, probs.log_prob(action), probs.entropy(), self.critic(state)
